bb0e169ed3280b79a43ae35bd307145c8d469537
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-14 16:56:09 +0530

    tall and short.

---
+++ js/tall&short.js
@@ -0,0 +1,13 @@
+var height = prompt("Enter the height");
+if (height<150)
+{
+  alert("short person");
+}
+else if(height>=150 && height <=165)
+{
+  alert("Average person");
+}
+else if (height>165 && height <=195)
+{
+  alert("Tallest person");
+}
